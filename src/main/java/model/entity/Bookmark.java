fb4a277a702b5e3230de2b1ad1869e9bd30bf2c4
nailboy <omomo010@gmail.com>
2022-07-04 22:35:59 +0900

    LoginForm 재구현 및 추가 수정

---
+++ src/main/java/model/entity/Bookmark.java
@@ -0,0 +1,35 @@
+﻿package model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+
+@Entity
+public class Bookmark {
+	
+	@Id
+	@Column(name="bookmarkId")
+	private int bookmarkId;
+	
+	private String CoinId;
+	
+	@ManyToOne(targetEntity = Member.class)
+	@JoinColumn(name="memberId")
+	private String memberId;
+	
+	
+}
